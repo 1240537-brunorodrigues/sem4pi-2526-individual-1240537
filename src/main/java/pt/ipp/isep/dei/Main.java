091e8f961df1043f3baf239811374742013b8878
meisgooddev <profissional.brjo@gmail.com>
2026-05-20 02:32:45 +0100

    061: Added base application

---
+++ src/main/java/pt/ipp/isep/dei/Main.java
@@ -0,0 +1,11 @@
+package pt.ipp.isep.dei;
+
+import pt.ipp.isep.dei.ui.console.MainMenuUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainMenuUI menu = new MainMenuUI();
+        menu.run();
+    }
+}
--- src/main/java/pt/ipp/isep/dei/ui/console/MainMenuUI.java
+++ src/main/java/pt/ipp/isep/dei/ui/console/MainMenuUI.java
@@ -1,4 +1,46 @@
 package pt.ipp.isep.dei.ui.console;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class MainMenuUI {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void run() {
+        int option;
+
+        do {
+            showMenu();
+            option = readOption();
+
+            switch (option) {
+                case 1 -> System.out.println("Login selected. Not implemented yet.");
+                case 0 -> System.out.println("Exiting application...");
+                default -> System.out.println("Invalid option. Please try again.");
+            }
+
+            System.out.println();
+        } while (option !=0);
+    }
+
+    private void showMenu() {
+        System.out.println("=================================");
+        System.out.println("        AlSafe Backoffice        ");
+        System.out.println("=================================");
+        System.out.println("1. Login");
+        System.out.println("0. Exit");
+        System.out.println("Choose an option: ");
+    }
+
+    private int readOption() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid option. Please insert a numeric value.");
+            scanner.next();
+            System.out.println("Choose an option: ");
+        }
+        int option = scanner.nextInt();
+        scanner.nextLine();
+        return option;
+    }
 }
